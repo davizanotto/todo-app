2dc5584dc57786a8eb30c9a4abef193646bce18b
Davi Zanotto <davizanotto@gmail.com>
2017-06-03 10:34:23 -0300

    Insert e delete funcionando com botões na tela e chamando css customizado

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -7,9 +7,14 @@ export default props => {
         const list = props.list || []
         return list.map(item => (
                 <tr key={item._id}>
-                    <td>{item.description}</td>
+                    <td className={item.done ? 'markedAsDone' : ''}>{item.description}</td>
                     <td>
-                        <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(item)} />
+                        <IconButton style='success' icon='check' hide={item.done}
+                            onClick={() => props.handleMarkAsDone(item)} />
+                        <IconButton style='warning' icon='undo'  hide={!item.done}
+                            onClick={() => props.handleMarkAsPending(item)} />
+                        <IconButton style='danger' icon='trash-o' hide={!item.done}
+                            onClick={() => props.handleRemove(item)} />
                     </td>
                 </tr>
         ))
