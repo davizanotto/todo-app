dd5605596cf4328b283b4b9545d5526cd7a4c9c0
Davi Zanotto <davizanotto@gmail.com>
2017-06-03 10:06:07 -0300

    Primeiras requisições mongodb através do axios - add e refresh

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import PageHeader from '../template/pageHeader'
+import Paragrafo from '../template/paragrafo'
 
 export default props => (
     <div>
         <PageHeader name='Sobre' small='Nós' />
 
-        <h2>Nossa História</h2>
-        <p>Lorem impsum</p>
-        
-        <h2>Missão e Visão</h2>
-        <p>Lorem impsum</p>
-        
-        <h2>Imprensa</h2>
-        <p>Lorem impsum</p>
+        <Paragrafo title='Nossa História'>Lorem impsum dor..</Paragrafo>
+        <Paragrafo title='Missão e Visão'>Lorem impsum dolen txarlen</Paragrafo>
+        <Paragrafo title='Imprensa'>Lorem impsum shift ctrl</Paragrafo>
     </div>
 )
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -21,20 +21,24 @@ module.exports = {
         new ExtractTextPlugin('app.css')
     ],
     module: {
-        loaders: [{
-            test: /.js[x]?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spread']
+        loaders: [
+            {
+                test: /.js[x]?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            }, 
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            }, 
+            {
+                test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+                loader: 'file'
             }
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        }, {
-            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-            loader: 'file'
-        }]
+        ]
     }
 }
